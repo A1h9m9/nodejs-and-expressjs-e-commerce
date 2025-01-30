0db36013795a7b4fe15ac925b7b5153f8f9cecd6
deer_code <85317165+A1h9m9@users.noreply.github.com>
2025-01-30 14:41:23 +0200

    Add files via upload

--- product.js
+++ product.js
@@ -1,66 +1,80 @@
-const mongoose = require('mongoose');
-
-const Schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required:true,
-        maxlength:100
-    },
-    description: {
-        type: String,
-        required:true,
-        maxlength:200
-    },
-    price: {
-        type: Number,
-        required:true,
-        min: [0.01, "Price must be greater than zero"],
-        validate: {
-          validator: (value) => Number(value.toFixed(2)) === value,
-          message: "Price can only have up to two decimal places",
-        },
-    },
-    category: {
-        type: String,
-        required:true
-    },
-    brand: {
-        type: String,
-        required:true,
-        maxlength: [50, "Brand name must not exceed 50 characters"],
-
-    },
-
-    rating: {
-        type: Number,
-        required:true,
-        min: [0, "Rating cannot be less than 0"],
-        max: [5, "Rating cannot exceed 5"],
-        validate: {
-          validator: (value) => Number(value.toFixed(1)) === value,
-          message: "Rating must have at most one decimal place",
-        },
-    },
-    image: {
-        type: String,
-        match: [
-            /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|webp))$/,
-            "Image URL must be a valid link to an image",
-          ],
-    },
-    inStock:{
-        type: String,
-        enum: ['inStock', 'outOfStock'],
-        default: 'inStock',
-        
-    },
-    isAdmin:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        
+const express = require('express');
+const router = express.Router();
+const prodb = require('../models/product');
+// Get a product by ID
+router.get('/api/product/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const product = await prodb.findById(id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-
 });
 
-const Product = mongoose.model('Product', Schema);
-module.exports = Product;
+// Create a new product
+router.post('/api/product/resinformations', async (req, res) => {
+    const { name, description, price, category, brand, rating, inStock } = req.body;
+
+    // Validate input here (e.g., use Joi or express-validator)
+
+    try {
+        const newProduct = new prodb({
+            name,
+            description,
+            price,
+            category,
+            brand,
+            rating,
+            inStock,
+        });
+        await newProduct.save();
+        res.status(201).json(newProduct);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Update a product by ID
+router.put('/api/product/:id', async (req, res) => {
+    const id = req.params.id;
+    const { name, description, price, category, brand, rating, inStock } = req.body;
+
+    try {
+        const updatedProduct = await prodb.findByIdAndUpdate(
+            id,
+            { name, description, price, category, brand, rating, inStock },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedProduct) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Delete a product by ID
+router.delete('/api/product/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const deletedProduct = await prodb.findByIdAndDelete(id);
+
+        if (!deletedProduct) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.status(200).json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+module.exports = router;
--- user.js
+++ user.js
@@ -1,65 +1,129 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const Router = express.Router();
+const userdb = require('../models/user');
 const bcrypt = require('bcrypt');
-const Schema = new mongoose.Schema({
+const crypto = require('crypto');
+const secret = 'code123';
+// Routes
 
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        validate: {
-            validator: async function (value) {
-                const existuser = await User.exists({ username: value });
-                return !existuser;
-            },
-            message: 'User already exist'
-        }
-    },
-    name: {
+// Test route all users
+Router.get('/allusers', async (req, res) => {
+    try {
+        const users = await userdb.find({});
+        res.json(users);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
 
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        validate: {
-            validator: async function (value) {
-                const existemail = await User.exists({ email: value });
-                return !existemail;
-            },
-            message: 'Email already exist'
-        }
-    },
-
-    password: {
-        type: String,
-        required: true,
-        minlength: 8,
-    },
-    role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user'
-    },
-
+   
 });
 
-// pre hash password middleware
-Schema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) { // Check if password was modified
-        try {
-            const hashedPassword = bcrypt.hash(this.password, 10); // Wait for the hash to complete
-            this.password = hashedPassword; // Assign the hashed password
-            next(); // Continue with the save
-        } catch (err) {
-            next(err); // Pass the error to next middleware
-        }
-    } else {
-        next(); // If no password modification, proceed
+// Signup route
+Router.post('/users/res', async (req, res) => {
+    const { username, name, email, password, role } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);// Hash password
+        const newUser = new userdb({ username, name, email, password: hashedPassword, role });
+
+
+        await newUser.save();
+        res.json(newUser);
+    } catch (err) {
+        res.status(400).send(err.message);
     }
 });
 
+// Login route
 
-const User = mongoose.model('User', Schema);
-module.exports = User;
+Router.post('/user/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await userdb.findOne({ email });
+        if (!user) return res.status(404).send('User not found');
+
+        // Compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).send('Invalid password');
+
+        // Save user information in session
+        req.session.user = {
+            id: user._id,
+            email: user.email,
+            role: user.role
+        };
+        res.json({ message: 'Login successful' });
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
+// Delete user
+Router.delete('/users/:id', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await userdb.findByIdAndDelete(userId);
+        if (!user) return res.status(404).send('User not found');
+        res.send(user);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
+// Protected route
+Router.get('/protected', (req, res) => {
+    if (!req.session.user) {
+        return res.status(401).send('You are not authenticated');
+    }
+    res.send(`مرحبًا ${req.session.user.email}. هذه صفحة محمية.`);
+})
+
+// Admin panel route
+Router.get('/admin/panel', authMiddleware, (req, res) => {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Forbidden: Admins only' });
+    }
+    res.send('Hello to the admin panel');
+});
+
+// Logout route
+Router.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).send('Failed to log out');
+        }
+        res.json({ message: 'Logout successful' });
+    });
+});
+
+// Update user
+Router.patch('/users/:id', async (req, res) => {
+    const userId = req.params.id;
+    const updates = req.body;
+
+    try {
+        const updatedUser = await userdb.findByIdAndUpdate(userId, updates, { new: true });
+        if (!updatedUser) return res.status(404).send('User not found');
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
+// Get user profile
+Router.get('/users/profile/:id', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await userdb.findById(userId);
+        if (!user) return res.status(404).send('User not found');
+        res.json(user);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
+// Export the router
+module.exports = Router;
